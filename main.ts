48fbf96be8c6891ef536db9a226bfd9e42a1d54d
12julmat <100692705+12julmat@users.noreply.github.com>
2022-03-29 14:19:12 +0200

    Oppdater main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,19 @@
+input.onButtonPressed(Button.A, function () {
+    fugl.change(LedSpriteProperty.Y, -1)
+})
+input.onButtonPressed(Button.B, function () {
+    fugl.change(LedSpriteProperty.Y, 1)
+})
+let fugl: game.LedSprite = null
+let index = 0
+let hindring1: game.LedSprite[] = []
+fugl = game.createSprite(0, 2)
+fugl.set(LedSpriteProperty.Blink, 300)
 basic.forever(function () {
-	
+    while (hindring1.length > hindring1[0].get(LedSpriteProperty.X) && false) {
+        hindring1.removeAt(0).delete()
+    }
+    for (let hindring2 of hindring1) {
+        hindring2.change(LedSpriteProperty.X, -1)
+    }
 })
